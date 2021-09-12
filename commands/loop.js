11126bf9ea09d010b375221ac24a4a08f4acfad3
Tmoemes <Tmoemes@users.noreply.github.com>
2021-09-12 22:38:24 +0200

    fix loop

--- commands/loop.js
+++ commands/loop.js
@@ -54,7 +54,7 @@ module.exports = {
       return void interaction.followUp({
         content: '❌ | No music is being played!',
       });
-    const loopMode = interaction.options.mode;
+    const loopMode = interaction.options.choices;
     const success = queue.setRepeatMode(loopMode);
     const mode = loopMode === QueueRepeatMode.TRACK ? "🔂" : loopMode === QueueRepeatMode.QUEUE ? "🔁" : "▶";
     return void interaction.followUp({
