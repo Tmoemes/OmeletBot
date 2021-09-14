91bea2bdf0f61072a6cf1fca9051ccadf1653df8
Tmoemes <Tmoemes@users.noreply.github.com>
2021-09-14 23:02:49 +0200

    possible fix for crash after leaving

--- index.js
+++ index.js
@@ -36,10 +36,12 @@ player.on('trackAdd', (queue, track) => {
 
 player.on('botDisconnect', queue => {
   queue.metadata.send('❌ | I was manually disconnected from the voice channel, clearing queue!');
+  queue.destroy();
 });
 
 player.on('channelEmpty', queue => {
   queue.metadata.send('❌ | Nobody is in the voice channel, leaving...');
+  queue.destroy();
 });
 
 player.on('queueEnd', queue => {
